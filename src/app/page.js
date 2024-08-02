37f1055d24e7cee7a644fd74591e531ca7121bb7
Paramasivan <paramasivan@adshi5.com>
2024-08-02 14:05:32 +0530

    GTM script added

--- src/app/page.js
+++ src/app/page.js
@@ -169,7 +169,26 @@ export default function Home() {
                 <meta property="og:description" content="DEPARTMENT OF PUBLIC HEALTH DENTISTRY SRM KATTANKULATHUR DENTAL COLLEGE & HOSPITAL. Workshop Details - Date: August 9th & 10th, 2024. Location: T.P. Ganesan Auditorium, Mini Hall 2" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://workshop.trysimmer.com/" />
+
+                        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PKH43898');
+          `,
+        }} />
+        {/* End Google Tag Manager */}
             </Head>
+
+                 {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PKH43898"
+        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
       <div className={styles.pagesize}>
         <div className={styles.clearfix}>
           <div className={styles.imageContainer}>
